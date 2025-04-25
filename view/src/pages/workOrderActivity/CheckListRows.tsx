fab366c8c5971d6cea0bb50e66b0e19713ac7bc6
birukax <birukax@gmail.com>
2025-04-25 12:19:48 +0300

    fixed a bug on the checklist page

---
+++ view/src/pages/workOrderActivity/CheckListRows.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import { TableRow, TableCell, Checkbox, TextField } from "@mui/material";
+
+const CheckListRows = ({ row, handleUpdateActivity }) => {
+  const [remark, setRemark] = useState(row.remark || "");
+  const [value, setValue] = useState(row.value || false);
+  useEffect(() => {
+    setRemark(row.remark || "");
+  }, [row.remark]);
+  useEffect(() => {
+    setValue(row.value || "");
+  }, [row.value]);
+  return (
+    <TableRow key={row.id}>
+      <TableCell align="left">{row.activity.name}</TableCell>
+      <TableCell align="left">
+        <Checkbox
+          checked={value}
+          onChange={(event) => {
+            setValue(event.target.checked);
+            handleUpdateActivity(row.id, "value", event.target.checked);
+          }}
+          name="value"
+          sx={{ transform: "scale(1.5)" }}
+        />
+      </TableCell>
+      <TableCell align="left">
+        <TextField
+          name="remark"
+          value={remark}
+          onChange={(event) => setRemark(event.target.value)}
+          onBlur={() => {
+            if (remark !== row.remark) {
+              handleUpdateActivity(row.id, "remark", remark);
+            }
+          }}
+        />
+      </TableCell>
+    </TableRow>
+  );
+};
+
+export default CheckListRows;
