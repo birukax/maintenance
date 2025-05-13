459d2d717b11f146613ad9ce26328b55ee7267dc
birukax <birukax@gmail.com>
2025-05-13 10:07:11 +0300

    working on the pagination"

--- api/main/settings.py
+++ api/main/settings.py
@@ -157,6 +157,8 @@ REST_FRAMEWORK = {
         "rest_framework.filters.SearchFilter",
         "rest_framework.filters.OrderingFilter",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 2,
 }
 
 ACCOUNT_AUTHENTICATION_METHOD = "username"
--- view/src/components/GenericListPage.tsx
+++ view/src/components/GenericListPage.tsx
@@ -20,14 +20,12 @@ import {
   ButtonGroup,
 } from "@mui/material";
 import RefreshIcon from "@mui/icons-material/Refresh";
-import { green } from "@mui/material/colors";
 
 export interface ColumnDefination {
   header: string;
   accessor?: string;
   renderCell?: (row) => React.ReactNode;
 }
-
 
 const getNestedValue = (obj, path) =>
   path.split(".").reduce((acc, part) => acc && acc[part], obj);
@@ -48,13 +46,13 @@ export const GenericListPage = ({
   getKey,
   searchFilter,
   keyWord,
-  setKeyWord
+  setKeyWord,
 }) => {
   const headers = columns.map((col) => col.header);
   const { tokens } = useSelector((state: AppState) => state.auth);
   const navigate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();  
-  
+  const [searchParams, setSearchParams] = useSearchParams();
+
   if (!tokens) {
     return <Typography>Unauthorized</Typography>;
   }
@@ -66,82 +64,123 @@ export const GenericListPage = ({
   if (typeof entityState.error === "string") {
     toast.error(entityState.error);
   } else {
-    if (entityState.error !== null && entityState.error.code!=="token_not_valid") {
+    if (
+      entityState.error !== null &&
+      entityState.error.code !== "token_not_valid"
+    ) {
       toast.error(JSON.stringify(entityState.error));
     }
   }
-
-  
+  console.log(entityState?.data, "entity");
   return (
     <>
-      <div className="flex gap-8 justify-between table-filters" style={{maxWidth:"100%",minWidth:"fit-content",maxHeight:"fit-content"}}>
+      <div
+        className="flex gap-8 justify-between table-filters"
+        style={{
+          maxWidth: "100%",
+          minWidth: "fit-content",
+          maxHeight: "fit-content",
+        }}
+      >
         <Typography variant="h5" className="font-bold ">
-          {title} {yearFilter && (<sub style={{fontSize:"large",color:"red",marginInlineStart:"1rem"}} className="year-margin"> <span style={{color:"black"}}> Year:</span>{searchParams.get("year__no")}</sub>)}
+          {title}{" "}
+          {yearFilter && (
+            <sub
+              style={{
+                fontSize: "large",
+                color: "red",
+                marginInlineStart: "1rem",
+              }}
+              className="year-margin"
+            >
+              {" "}
+              <span style={{ color: "black" }}> Year:</span>
+              {searchParams.get("year__no")}
+            </sub>
+          )}
         </Typography>
-        <div style={{maxHeight:"fit-content"}}>
-
-          {
-            searchFilter && 
-            <div style={{display:"flex",gap:"0",alignItems:"center",justifyContent:"center"}}>
+        <div style={{ maxHeight: "fit-content" }}>
+          {searchFilter && (
+            <div
+              style={{
+                display: "flex",
+                gap: "0",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
               <TextField
-            label="Search"
-            name="search"
-            className="mb-8"
-            variant="outlined"
-            size="small"
-            fullWidth
-            value={keyWord}
-            onChange={(e)=>setKeyWord(e.target.value)}
-            sx={{minWidth:"250px",
-              border:"none",
-              "& .MuiOutlinedInput-root": {
-                borderTopRightRadius: 0,
-                borderBottomRightRadius: 0,
-              display:"flex",
-              },
-            
-            }}
-            
-          />
-              <Button variant="contained" onClick={()=>searchFilter("search",keyWord)} sx={{ mr: 1,borderTopLeftRadius:0,borderBottomLeftRadius:0,height:"40px",border:"1px solid #5d4037"}}>
-                <SearchIcon/>
+                label="Search"
+                name="search"
+                className="mb-8"
+                variant="outlined"
+                fullWidth
+                value={keyWord}
+                onChange={(e) => setKeyWord(e.target.value)}
+                sx={{
+                  minWidth: "250px",
+                  maxHeight: "40px",
+                  border: "none",
+                  "& .MuiOutlinedInput-root": {
+                    borderTopRightRadius: 0,
+                    borderBottomRightRadius: 0,
+                    height: "100%",
+                    maxHeight: "40px",
+                  },
+                }}
+              />
+              <Button
+                variant="contained"
+                onClick={() => searchFilter("search", keyWord)}
+                sx={{
+                  mr: 1,
+                  borderTopLeftRadius: 0,
+                  borderBottomLeftRadius: 0,
+                  height: "40px",
+                  border: "1px solid #5d4037",
+                }}
+              >
+                <SearchIcon />
               </Button>
             </div>
-            
-          }
+          )}
           {yearFilter && (
-            
-               <TextField
+            <TextField
               select
               onChange={(e) => yearFilter("year__no", e.target.value)}
               variant="outlined"
               size="small"
               focused
-              sx={{ marginRight: "8px", minWidth: "130px",outlineColor:"blue",maxHeight:"40px"}}
+              sx={{
+                marginRight: "8px",
+                minWidth: "130px",
+                outlineColor: "blue",
+                maxHeight: "40px",
+              }}
               value={searchParams.get("year__no")}
               className="year-filter"
             >
               {Array.from({ length: 5 }, (_, i) => 2025 + i).map((year) => (
-                <MenuItem key={year} value={year} sx={{fontWeight:"bold" }}>
+                <MenuItem key={year} value={year} sx={{ fontWeight: "bold" }}>
                   {year}
                 </MenuItem>
               ))}
             </TextField>
-           
           )}
           {createRoute && (
             <Button
               component={Link}
               to={createRoute}
               variant="outlined"
-              sx={{ mr: 1,padding:".5rem 2rem",maxHeight:"40px" }}
+              sx={{ mr: 1, padding: ".5rem 2rem", maxHeight: "40px" }}
             >
               New
             </Button>
           )}
           {onEdit &&
             (searchParams.get("edit") === false ||
-              searchParams.get("edit") === null) && entityState.data?.length>0 &&(
+              searchParams.get("edit") === null) &&
+            entityState.data?.count > 0 && (
               <Button variant="outlined" onClick={onEdit} sx={{ mr: 1 }}>
                 Edit
               </Button>
@@ -152,7 +191,7 @@ export const GenericListPage = ({
             onClick={onRefresh}
             disabled={entityState.loading}
             className="btn-refresh"
-            sx={{maxHeight:"40px"}}
+            sx={{ maxHeight: "40px" }}
           >
             Refresh
           </Button>
@@ -165,88 +204,95 @@ export const GenericListPage = ({
         </Typography>
       )}
 
-<div className="table-container">
-
-      <Table sx={{ minWidth: 650,maxHeight:"40px"}} aria-label={`${title} table`} className="table">
-        <TableHead>
-          <TableRow>
-            {headers.map((header) => (
-              <TableCell key={header}>
-                <Typography noWrap>{header}</Typography>
-              </TableCell>
-            ))}
-            {(hasDetail || hasApproval) && (
-              <TableCell align="left">
-                <Typography noWrap>Action</Typography>
-              </TableCell>
-            )}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {entityState.data && entityState.data.length > 0
-            ? entityState.data.map((row) => (
-                <TableRow
-                  key={getKey(row)}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  {columns.map((col) => {
-                    return (
-                      <TableCell key={col.header} component="th" scope="row" align="left" >
-                        {col.renderCell
-                          ? col.renderCell(row)
-                          : col.accessor
-                          ? String(getNestedValue(row, String(col.accessor)))
-                          : "N/A"}
-                      </TableCell>
-                    );
-                  })}
-
-                  {hasDetail && (
-                    <TableCell align="left">
-                      <Button
-                        component={Link}
-                        to={`${detailRouteBase}/${getKey(row)}`}
-                        size="small"
-                      >
-                        Detail
-                      </Button>
-                    </TableCell>
-                  )}
-                  {hasApproval && row.status === "PENDING" && (
-                    <TableCell align="left">
-                      <ButtonGroup
-                        variant="contained"
-                        size="small"
-                        sx={{ display: "flex", gap: 1, boxShadow: 0 }}
-                      >
-                        <Button
-                          onClick={() => onApprove(row.id)}
-                          disabled={entityState.loading}
-                        >
-                          Approve
-                        </Button>
-                        <Button
-                          onClick={() => onReject(row.id)}
-                          disabled={entityState.loading}
-                        >
-                          Reject
-                        </Button>
-                      </ButtonGroup>
-                    </TableCell>
-                  )}
-                </TableRow>
-              ))
-            : !entityState.loading && (
-                <TableRow>
-                  <TableCell colSpan={headers.length + 1} align="center">
-                    No data available.
-                  </TableCell>
-                </TableRow>
+      <div className="table-container">
+        <Table
+          sx={{ minWidth: 650, maxHeight: "40px" }}
+          aria-label={`${title} table`}
+          className="table"
+        >
+          <TableHead>
+            <TableRow>
+              {headers.map((header) => (
+                <TableCell key={header}>
+                  <Typography noWrap>{header}</Typography>
+                </TableCell>
+              ))}
+              {(hasDetail || hasApproval) && (
+                <TableCell align="left">
+                  <Typography noWrap>Action</Typography>
+                </TableCell>
               )}
-        </TableBody>
-      </Table>
-</div>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {entityState?.data?.results && entityState?.data?.count > 0
+              ? entityState?.data?.results?.map((row) => (
+                  <TableRow
+                    key={getKey(row)}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    {columns.map((col) => {
+                      return (
+                        <TableCell
+                          key={col.header}
+                          component="th"
+                          scope="row"
+                          align="left"
+                        >
+                          {col.renderCell
+                            ? col.renderCell(row)
+                            : col.accessor
+                            ? String(getNestedValue(row, String(col.accessor)))
+                            : "N/A"}
+                        </TableCell>
+                      );
+                    })}
 
+                    {hasDetail && (
+                      <TableCell align="left">
+                        <Button
+                          component={Link}
+                          to={`${detailRouteBase}/${getKey(row)}`}
+                          size="small"
+                        >
+                          Detail
+                        </Button>
+                      </TableCell>
+                    )}
+                    {hasApproval && row.status === "PENDING" && (
+                      <TableCell align="left">
+                        <ButtonGroup
+                          variant="contained"
+                          size="small"
+                          sx={{ display: "flex", gap: 1, boxShadow: 0 }}
+                        >
+                          <Button
+                            onClick={() => onApprove(row.id)}
+                            disabled={entityState.loading}
+                          >
+                            Approve
+                          </Button>
+                          <Button
+                            onClick={() => onReject(row.id)}
+                            disabled={entityState.loading}
+                          >
+                            Reject
+                          </Button>
+                        </ButtonGroup>
+                      </TableCell>
+                    )}
+                  </TableRow>
+                ))
+              : !entityState.loading && (
+                  <TableRow>
+                    <TableCell colSpan={headers} align="center">
+                      No data available.
+                    </TableCell>
+                  </TableRow>
+                )}
+          </TableBody>
+        </Table>
+      </div>
     </>
   );
 };
