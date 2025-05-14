330f3d4994a2b8588d4728ca463b490062405b35
Wanofi Israel <wanofi@qualabels.local>
2025-05-14 10:12:03 +0300

    added profie

---
+++ view/src/pages/profile/Create.tsx
@@ -0,0 +1,102 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { createProfile } from "../../store/slices/profileSlice";
+import api from "../../utils/api";
+import {
+  TextField,
+  Button,
+  Typography,
+  Container,
+  CircularProgress,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Box,
+} from "@mui/material";
+import { toast } from "react-toastify";
+const Create = () => {
+  const [formData, setFormData] = useState({
+    code: "",
+    name: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+    try {
+      await dispatch(createProfile(formData)).unwrap();
+      toast.success("Profile created successfully");
+      navigate("/profiles");
+    } catch (err) {
+      toast.error("Error creating Profile");
+      setError(err.response?.data.detail || err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <Container className="flex flex-col items-center justify-center min-h-full ">
+      <Typography variant="h4" className="mb-6 text-gray-800">
+        Create Profile
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        className="form-gap"
+      >
+        <TextField
+          label="Code"
+          name="code"
+          className="mb-8"
+          variant="outlined"
+          fullWidth
+          value={formData.code}
+          onChange={handleChange}
+          required
+          disabled={loading}
+        />
+
+        <TextField
+          label="Name"
+          name="name"
+          className="mb-8"
+          variant="outlined"
+          fullWidth
+          value={formData.name}
+          onChange={handleChange}
+          required
+          disabled={loading}
+        />
+
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          fullWidth
+          disabled={loading}
+          className="mt-4"
+        >
+          {loading ? <CircularProgress size={24} /> : "Create Profile"}
+        </Button>
+        {error && (
+          <Typography variant="body2" className="mt-4 text-red-500">
+            {error}
+          </Typography>
+        )}
+      </Box>
+    </Container>
+  );
+};
+
+export default Create;
