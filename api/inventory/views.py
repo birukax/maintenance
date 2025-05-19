e82585f32e3e536dcfbc21b1f8e929b2de356227
birukax <birukax@gmail.com>
2025-05-19 14:29:12 +0300

    update on the revaluate stock function on inventory view

--- api/inventory/views.py
+++ api/inventory/views.py
@@ -101,40 +101,90 @@ class InventoryViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, methods=["GET"])
     def revaluate_stock(self, request):
-        inventories = Inventory.objects.all()
-        purchases = Request.objects.all()
-        consumptions = Consumption.objects.all()
-        returns = Return.objects.filter(used=False)
-        for i in inventories:
-            if purchases.filter(item=i.item).exists():
-                i.purchased_quantity = (
-                    purchases.filter(item=i.item)
-                    .aggregate(total=Sum("received_quantity"))
-                    .get("total")
-                )
-            else:
-                i.purchased_quantity = 0
-            if consumptions.filter(item=i.item).exists():
-                i.consumed_quantity = (
-                    consumptions.filter(item=i.item)
-                    .aggregate(total=Sum("quantity"))
-                    .get("total")
-                )
-            else:
-                i.consumed_quantity = 0
-            if returns.filter(item=i.item).exists():
-                i.returned_quantity = (
-                    returns.filter(item=i.item)
-                    .aggregate(total=Sum("quantity"))
-                    .get("total")
-                )
-            else:
-                i.returned_quantity = 0
-            i.save()
-            i.balance = i.purchased_quantity - i.consumed_quantity + i.returned_quantity
-            i.save()
-        serializer = InventorySerializer(inventories, many=True)
+        all_purchases = Request.objects.values("item_id").annotate(
+            total_received=Sum("received_quantity")
+        )
+        all_consumptions = Consumption.objects.values("item_id").annotate(
+            total_consumed=Sum("quantity")
+        )
+        all_returns = (
+            Return.objects.filter(used=False)
+            .values("item_id")
+            .annotate(total_returned=Sum("quantity"))
+        )
+        purchase_map = {p["item_id"]: p["total_received"] for p in all_purchases}
+        consumptions_map = {c["item_id"]: c["total_consumed"] for c in all_consumptions}
+        returns_map = {r["item_id"]: r["total_returned"] for r in all_returns}
+
+        inventories_to_update = Inventory.objects.all()
+        updated_inventory_instances = []
+
+        for inv_item in inventories_to_update:
+            inv_item.purchased_quantity = purchase_map.get(inv_item.item_id, 0) or 0
+            inv_item.consumed_quantity = consumptions_map.get(inv_item.item_id, 0) or 0
+            inv_item.returned_quantity = returns_map.get(inv_item.item_id, 0) or 0
+
+            inv_item.balance = (
+                inv_item.purchased_quantity
+                - inv_item.consumed_quantity
+                + inv_item.returned_quantity
+            )
+
+            updated_inventory_instances.append(inv_item)
+
+        if updated_inventory_instances:
+            Inventory.objects.bulk_update(
+                updated_inventory_instances,
+                [
+                    "purchased_quantity",
+                    "consumed_quantity",
+                    "returned_quantity",
+                    "balance",
+                ],
+            )
+        queryset = self.filter_queryset(self.get_queryset())
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+        # inventories = Inventory.objects.all()
+        # purchases = Request.objects.all()
+        # consumptions = Consumption.objects.all()
+        # returns = Return.objects.filter(used=False)
+        # for i in inventories:
+        #     if purchases.filter(item=i.item).exists():
+        #         i.purchased_quantity = (
+        #             purchases.filter(item=i.item)
+        #             .aggregate(total=Sum("received_quantity"))
+        #             .get("total")
+        #         )
+        #     else:
+        #         i.purchased_quantity = 0
+        #     if consumptions.filter(item=i.item).exists():
+        #         i.consumed_quantity = (
+        #             consumptions.filter(item=i.item)
+        #             .aggregate(total=Sum("quantity"))
+        #             .get("total")
+        #         )
+        #     else:
+        #         i.consumed_quantity = 0
+        #     if returns.filter(item=i.item).exists():
+        #         i.returned_quantity = (
+        #             returns.filter(item=i.item)
+        #             .aggregate(total=Sum("quantity"))
+        #             .get("total")
+        #         )
+        #     else:
+        #         i.returned_quantity = 0
+        #     i.save()
+        #     i.balance = i.purchased_quantity - i.consumed_quantity + i.returned_quantity
+        #     i.save()
+        # serializer = InventorySerializer(inventories, many=True)
+        # return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class ConsumptionViewSet(viewsets.ModelViewSet):
